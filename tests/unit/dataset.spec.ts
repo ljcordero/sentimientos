8a3008ffd2a2a3a50f21865e445f4c98b83ba92b
luis.hernandez <luis.hernandez@DICTC-LP06W10>
2020-03-05 13:19:54 -0400

    datasets test added to find the accuracy of the models used.

---
+++ tests/unit/dataset.spec.ts
@@ -0,0 +1,80 @@
+import datasetDivider from '@/utils/divider';
+import Dataset, { TweetTASS } from '@/core/models/Dataset';
+import NaiveBayesService from '@/core/services/NaiveBayesService';
+import Trainer from '@/core/services/interfaces/Trainer';
+import Classifier from '@/core/services/interfaces/Classifier';
+import AfinnService from '@/core/services/AfinnService';
+
+const datasets: Dataset[] = [
+  TweetTASS,
+];
+
+function getModels(): (Trainer & Classifier)[] {
+  return [
+    new NaiveBayesService(),
+  ];
+}
+
+const DATASET_TRAIN_PROPORTION = 70;
+
+describe('datasets validations', () => {
+  it('models accuracy', () => {
+    datasets.forEach((config) => {
+      const dataset = datasetDivider(config, DATASET_TRAIN_PROPORTION);
+
+      console.log(`Dataset: ${config.source}`);
+
+      getModels().forEach((model) => {
+        console.log(`Model: ${model.constructor.name}`);
+
+        const validationResult = {
+          success: 0,
+          total: 0,
+        };
+
+        dataset.train.forEach((data) => {
+          model.train(data[config.value], data[config.key]);
+        });
+
+        dataset.test.forEach((data) => {
+          const result = model.classify(data[config.value]);
+
+          validationResult.total += 1;
+
+          if (result === data[config.key]) {
+            validationResult.success += 1;
+          }
+        });
+
+        console.log(`Accuracy: ${(validationResult.success / validationResult.total) * 100}`);
+      });
+    });
+  }).timeout(Number.MAX_VALUE);
+
+  it('afinn accuracy', () => {
+    datasets.forEach((config) => {
+      const dataset = datasetDivider(config, DATASET_TRAIN_PROPORTION);
+
+      console.log(`Dataset: ${config.source}`);
+
+      const model = new AfinnService();
+
+      const validationResult = {
+        success: 0,
+        total: 0,
+      };
+
+      dataset.train.forEach((data) => {
+        const result = model.classify(data[config.value]);
+
+        validationResult.total += 1;
+
+        if (result === data[config.key]) {
+          validationResult.success += 1;
+        }
+      });
+
+      console.log(`Accuracy: ${(validationResult.success / validationResult.total) * 100}`);
+    });
+  }).timeout(Number.MAX_VALUE);
+});
